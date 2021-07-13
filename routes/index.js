aafe2596c29f0e269557a21aa7220911e68cde59
Henry <hencan.1997@gmail.com>
2021-07-12 22:38:32 -0400

    Added loop of message array on index view

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'my mini messageboard thing', messages: messages});
 });
 
 module.exports = router;
